12710892a0d64703a5c3e173fa6b4082496bd1a8
minaho_watabe <minahohk@hotmail.com>
2018-07-20 06:58:19 +0000

    favoritespdf

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,29 @@
 class FavoritesController < ApplicationController
   def index
     @favorites_plans = current_user.favorite_plans
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.pdf do
+        # 詳細画面のHTMLを取得
+        html = render_to_string template: "favorites/index"
+
+        # PDFKitを作成
+        pdf = PDFKit.new(html, encoding: "UTF-8")
+
+        pdf.stylesheets << "#{Rails.root}/app/assets/stylesheets/plans.scss"
+        pdf.stylesheets << "#{Rails.root}/app/assets/stylesheets/bootstrap.min.css"
+        
+        # 画面にPDFを表示する
+        # to_pdfメソッドでPDFファイルに変換する
+        # 他には、to_fileメソッドでPDFファイルを作成できる
+        # disposition: "inline" によりPDFはダウンロードではなく画面に表示される
+        send_data pdf.to_pdf,
+          filename:    "#{@favorites_plans.id}.pdf",
+          type:        "application/pdf",
+          disposition: "inline"
+      end
+     end
   end
 
   def create
